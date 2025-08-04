5ac1aa37acd8998d4772058e5e4c0a457dc4d5c6
erikstiven <erikquisnia@gmail.com>
2025-08-04 17:54:01 -0500

    init: Frontend con Next.js y Tailwind

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcionando 🎉
+        <div>
+          <span className="text-green-500">Configuração</span> concluída com sucesso!
+          <span className="text-red-500">🚀ss</span>
+          <span className="text-yellow-500">🔥ss</span>
+          <span className="rounded-full">🎉ss</span>
+        </div>
+      </h1>
+    </div>
+  );
+}
